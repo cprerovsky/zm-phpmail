6a565562346d924304ed0e6b73399e913743b67a
Clemens Prerovsky <c.prerovsky@gmail.com>
2012-05-27 21:17:32 +0200

    improved mail by adding some more pictures of the last alarm

--- zm_alert.php
+++ zm_alert.php
@@ -18,37 +18,65 @@ while ($row = mysql_fetch_assoc($res)) {
 	`echo ${row["Id"]} > zm_event`;
 
 	$path = $img_path . date("y/m/d/H/i/s/", $row["ts"]);
-	$filename = "005-capture.jpg";
 
-	mail_attachment($path, $filename, 'c.prerovsky@gmail.com', 'zone.surveillance@gmail.com', 
-		'ZoneMinder', 'ZoneMinder Alert', 'Alert @' . $row['StartTime']);
+	$m = new SimpleMail("zone.surveillance@gmail.com", "ZoneMinder", "ZoneMinder Alert", 'Alert @' . $row['StartTime']);
+	
+	$m->attach($path, "001-capture.jpg");
+	$m->attach($path, "010-capture.jpg");
+	$m->attach($path, "020-capture.jpg");
+	$m->attach($path, "030-capture.jpg");
+	$m->attach($path, "040-capture.jpg");
+	$m->attach($path, "050-capture.jpg");
+
+	$m->send("c.prerovsky@gmail.com");
 }
 
-function mail_attachment($path, $filename, $mailto, $from_mail, $from_name, $subject, $message) {
-    $file = $path . $filename;
-    $replyto = $from_mail;
-    $file_size = filesize($file);
-    $handle = fopen($file, "r");
-    $content = fread($handle, $file_size);
-    fclose($handle);
-    $content = chunk_split(base64_encode($content));
-    $uid = md5(uniqid(time()));
-    $name = basename($file);
-    $header = "From: ".$from_name." <".$from_mail.">\r\n";
-    $header .= "Reply-To: ".$replyto."\r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-    $header .= "This is a multi-part message in MIME format.\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-    $header .= $message."\r\n\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-Type: application/octet-stream; name=\"$filename\"\r\n"; // use different content types here
-    $header .= "Content-Transfer-Encoding: base64\r\n";
-    $header .= "Content-Disposition: attachment; filename=\"$filename\"\r\n\r\n";
-    $header .= $content."\r\n\r\n";
-    $header .= "--".$uid."--";
-    mail($mailto, $subject, "", $header);
+class SimpleMail {
+	var $header;
+	var $uid;
+	var $subject;
+
+	function __construct($from_mail, $from_name, $subject, $message) {
+		$this->subject = $subject;
+		$this->uid = md5(uniqid(time()));
+
+		$this->header = "From: ".$from_name." <".$from_mail.">\r\n";
+    		$this->header .= "Reply-To: $from_mail\r\n";
+    		$this->header .= "MIME-Version: 1.0\r\n";
+    		$this->header .= "Content-Type: multipart/mixed; boundary=\"".$this->uid."\"\r\n\r\n";
+    		$this->header .= "This is a multi-part message in MIME format.\r\n";
+    		$this->header .= "--".$this->uid."\r\n";
+		$this->header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+    		$this->header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+    		$this->header .= $message."\r\n\r\n";
+    		$this->header .= "--".$this->uid."\r\n";
+	}
+
+	function send($to) {
+		mail($to, $this->subject, "", $this->header);
+	}
+
+	function attach($path, $filename) {
+		$file = $path . $filename;
+		if (!file_exists($file)) {
+			return;
+		}
+
+		echo "Attaching file $path$filename\n";
+
+    		$file_size = filesize($file);
+		$handle = fopen($file, "r");
+    		$content = fread($handle, $file_size);
+    		fclose($handle);
+    		$content = chunk_split(base64_encode($content));
+
+		$this->header .= "Content-Type: image/jpeg; name=\"$filename\"\r\n"; // use different content types here
+    		$this->header .= "Content-Transfer-Encoding: base64\r\n";
+    		$this->header .= "Content-Disposition: attachment; filename=\"$filename\"\r\n\r\n";
+    		$this->header .= $content."\r\n\r\n";
+    		$this->header .= "--".$this->uid."\r\n";
+	}
 }
+
+
 ?>
